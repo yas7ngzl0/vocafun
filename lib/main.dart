50a297278915923575b00ee6f21d2f7921255fb4
yas7ngzl0 <yasingzl77@gmail.com>
2023-11-25 08:18:12 +0300

    bottom sheet has been set

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:vocafun/level_selection_page.dart';
 import 'package:vocafun/second_page.dart';
 
 void main() {
@@ -210,11 +209,9 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
           4,
           (index) => GestureDetector(
             onTap: () {
-              // Kutuya tıklandığında, dil seviyesi seçimi sayfasına geçiş yap
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) => LanguageSelectionPage()),
+              showModalBottomSheet(
+                context: context,
+                builder: (context) => LanguageSelectionWidget(),
               );
             },
             child: Padding(
@@ -260,5 +257,94 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
         ),
       ),
     );
+  }
+}
+
+class LanguageSelectionWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.5,
+      width: MediaQuery.of(context).size.width,
+      child: Column(
+        children: [
+          Text(
+            'Dil Seviyesi Seçimi',
+            style: TextStyle(fontSize: 18),
+          ),
+          SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              LanguageLevelButton(level: 'A1'),
+              LanguageLevelButton(level: 'A2'),
+            ],
+          ),
+          SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              LanguageLevelButton(level: 'B1'),
+              LanguageLevelButton(level: 'B2'),
+            ],
+          ),
+          SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              LanguageLevelButton(level: 'C1'),
+              LanguageLevelButton(level: 'C2'),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class LanguageLevelButton extends StatelessWidget {
+  final String level;
+
+  LanguageLevelButton({required this.level});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {
+        // Seviye seçildiğinde yapılacak işlemler
+        Navigator.pop(context); // Bottom sheet'i kapat
+      },
+      style: ElevatedButton.styleFrom(
+        backgroundColor: getButtonColor(
+            level), // İstediğiniz rengi buradan belirleyebilirsiniz
+        shape:
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+        minimumSize: Size(100, 100), // Kare boyutu
+      ),
+      child: Text(
+        level,
+        style: TextStyle(fontSize: 18, color: Colors.white),
+      ),
+    );
+  }
+}
+
+Color getButtonColor(String level) {
+  // Her bir seviye için farklı renkleri döndür
+  switch (level) {
+    case 'A1':
+      return Colors.green.shade400;
+    case 'A2':
+      return Colors.blue.shade400;
+    case 'B1':
+      return Colors.yellow.shade400;
+    case 'B2':
+      return Colors.orange.shade400;
+    case 'C1':
+      return Colors.red.shade400;
+    case 'C2':
+      return Colors.purple.shade400;
+    default:
+      return Colors.blue.shade400; // Varsayılan renk
   }
 }
