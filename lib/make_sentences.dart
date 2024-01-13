b9a79663021b8b2a9ed72f1db702ad7220f54c82
yas7ngzl0 <yasingzl77@gmail.com>
2024-01-13 12:45:19 +0300

    words inside the boxes are now different from each other

--- lib/make_sentences.dart
+++ lib/make_sentences.dart
@@ -59,6 +59,9 @@ class MakeSentencesScreen extends StatelessWidget {
 
 
 
+import 'dart:convert';
+import 'dart:math';
+
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
@@ -72,8 +75,74 @@ class MakeSentencesScreen extends StatefulWidget {
 }
 
 class _MakeSentencesScreenState extends State<MakeSentencesScreen> {
-  List<String> words = ["I", "love", "play", "computer"];
+  List<String> words = ["I", "love", "play", "computer"," "," "," "," "];
   int currentIndex = 0;
+
+
+  //kutularda kullanılmak üzere keliemelri içerecek listeler
+  List<String> selectedSentenceWords = [];
+  List<String> randomWords = [];
+  List<bool> isDeleteCalledList = [false, false, false]; // Her satır için bayrakları içeren liste
+
+
+
+  @override
+  void initState() {
+    super.initState();
+    loadSentences();
+  }
+
+  Future<void> loadSentences() async {
+    String content = await DefaultAssetBundle.of(context).loadString('assets/sentences.txt');
+    List<String> sentences = LineSplitter.split(content).toList();
+
+    // Örnek olarak ilk cümleyi seçtik, istediğiniz başka bir algoritma kullanabilirsiniz.
+    Random random = new Random();
+    int sentencesIndex  =random.nextInt(sentences.length);
+    String selectedSentence = sentences[130];
+    //131 --> denediğim cümle indeksi
+
+    // Seçilen cümleyi kelimelere ayırarak listeye ekle
+    selectedSentenceWords = selectedSentence.split(' ');
+    selectedSentenceWords.shuffle();
+
+    // Diğer cümlelerden rastgele kelimeleri seç
+    randomWords = getRandomWords(sentences, selectedSentenceWords.length);
+
+    // İlk liste karışık, ikinci liste rastgele seçilmiş kelimelerle dolduruldu
+    setState(() {});
+  }
+
+
+
+
+  List<String> getRandomWords(List<String> sentences, int count) {
+    List<String> result = [];
+    Random random = Random();
+    result.addAll(selectedSentenceWords);
+
+    while (result.length < 12) {
+      String randomWord;
+      do {
+        // Rastgele bir cümle seç
+        String randomSentence = sentences[random.nextInt(sentences.length)];
+        // Cümleyi kelimelere ayır ve rastgele bir kelime seç
+        List<String> randomSentenceWords = randomSentence.split(' ');
+        randomWord = randomSentenceWords[random.nextInt(randomSentenceWords.length)];
+      } while (result.contains(randomWord)); // Aynı kelimeyi iki kez seçmemek için kontrol
+
+      result.add(randomWord);
+    }
+    result.shuffle();
+
+    return result;
+  }
+
+
+
+
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -156,24 +225,26 @@ class _MakeSentencesScreenState extends State<MakeSentencesScreen> {
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: List.generate(
-                      words.length,
-                          (index) => buildAnswerBox(index),
+                      4,
+                          (index) => buildAnswerBox(index,0),
                     ),
                   ),
+
                   SizedBox(height: 10),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+
                     children: List.generate(
-                      words.length,
-                          (index) => buildAnswerBox(index),
+                      4,
+                          (index) => buildAnswerBox(index,1),
                     ),
                   ),
                   SizedBox(height:10),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: List.generate(
-                      words.length,
-                          (index) => buildAnswerBox(index),
+                      4,
+                          (index) => buildAnswerBox(index,2),
                     ),
                   )
                 ],
@@ -187,7 +258,25 @@ class _MakeSentencesScreenState extends State<MakeSentencesScreen> {
     );
   }
 
-  Widget buildAnswerBox(int index) {
+
+
+
+
+  Widget buildAnswerBox(int index,int counter) {
+    /*if (index < 0 || index >= selectedSentenceWords.length + randomWords.length) {
+      // İndeks sınırların dışında, boş bir konteyner döndür
+      return Container();
+    }*/
+
+    if (randomWords.length > 4 && counter != 0 && !isDeleteCalledList[counter]) {
+      //randomWords = deleteFirst4Words(randomWords);
+      deleteFirst4Words(randomWords);
+      isDeleteCalledList[counter] = true; // delete fonksiyonunun bir kez çağrıldığını belirt
+    }
+
+    String word;
+    word = randomWords[index];
+
     return GestureDetector(
       onTap: () {
         setState(() {
@@ -195,7 +284,7 @@ class _MakeSentencesScreenState extends State<MakeSentencesScreen> {
         });
       },
       child: Container(
-        width: MediaQuery.of(context).size.width / 5,
+        width: MediaQuery.of(context).size.width / 5,  // 4 sütun
         height: 30,
         decoration: BoxDecoration(
           color: currentIndex == index ? Colors.green : Colors.white,
@@ -204,11 +293,34 @@ class _MakeSentencesScreenState extends State<MakeSentencesScreen> {
         margin: EdgeInsets.all(5),
         child: Center(
           child: Text(
-            words[index],
-            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            word,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 9 < selectedSentenceWords[index].length ? 12 : 15,
+            ),
           ),
         ),
       ),
     );
   }
+
+
+
+  /*List<String> deleteFirst4Words(List<String> randomWords) {
+    for (int i = 0; i < 4; i++) {
+      randomWords.removeAt(i);
+    }
+    return randomWords;
+  }*/
+
+
+  List<String> deleteFirst4Words(List<String> randomWords) {
+    if (randomWords.length >= 4) {
+      randomWords.removeRange(0, 4);
+    }
+    return randomWords;
+  }
+
+
+
 }
