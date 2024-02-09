e3432c0e790a949b69137ee393d964f3572f3a9e
yas7ngzl0 <yasingzl77@gmail.com>
2024-02-09 17:23:01 +0300

    taboo screen has been finished for now

--- lib/tabu.dart
+++ lib/tabu.dart
@@ -6,6 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 void main() {
+  ///BU Oyuna da istersen can ve skor özelliği getirebilirsin eğleceli olabilir
   runApp(TabuScreen());
 }
 
@@ -17,8 +18,10 @@ class TabuScreen extends StatefulWidget {
 class _TabuScreenState extends State<TabuScreen> {
   List<String> words = [];
   List<String> selectedSentenceWords = [];
-  List<String> UsingWords = [];
+  List<String> usingWords = [];
   late String selectedSentence;
+  int currentIndex = 6;
+  bool isAnswerTrue = false;
 
   @override
   void initState() {
@@ -31,28 +34,49 @@ class _TabuScreenState extends State<TabuScreen> {
       String content = await DefaultAssetBundle.of(context).loadString('assets/tabuwords.txt');
       words = LineSplitter.split(content).toList();
 
-      Random random = new Random();
+      Random random = Random();
       int sentencesIndex = random.nextInt(words.length);
       selectedSentence = words[sentencesIndex];
       selectedSentenceWords = selectedSentence.split(' ');
+      // Karıştırmayı burada yapıyoruz
+      // Çünkü bu liste doğruluk kontrolü için lazım olabilir
+      selectedSentenceWords.shuffle();
+      usingWords.clear();
       for (String word in selectedSentenceWords) {
         if (word.startsWith('!')) {
-          UsingWords.add(word.substring(1)); // Başındaki ünlem işaretini kaldır
+          usingWords.add(word.substring(1)); // Başındaki ünlem işaretini kaldır
         } else {
-          UsingWords.add(word);
+          usingWords.add(word);
         }
       }
-
-     UsingWords.shuffle();
 
       setState(() {}); // State'i güncelle
     } catch (e) {
       print('Dosya okuma hatası: $e');
     }
+    currentIndex = 6;
   }
 
+  void checkAndLoadNextWord() {
+    Future.delayed(Duration(milliseconds: 500), () {
+      if (selectedSentenceWords[currentIndex].startsWith('!')) {
+        // Eğer seçilen kelimenin başında ünlem işareti varsa, arka plan rengini yeşil yap
+        // ve yeni bir kelimeye geçmek için loadWords fonksiyonunu çağır
+
+        setState(() {
+
+        });
+        loadWords(); // Yeni bir kelimeye geç
+      } else {
+        // Eğer seçilen kelimenin başında ünlem işareti yoksa, sadece yeni bir kelimeye geç
+        //loadWords(); // Yeni bir kelimeye geç
+      }
+    });
+  }
+
+
   @override
-  Widget build(Object context) {
+  Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.green[300],
@@ -68,26 +92,50 @@ class _TabuScreenState extends State<TabuScreen> {
         body: Stack(
           alignment: Alignment.center,
           children: List.generate(5, (index) {
+            Color backgroundColor = Colors.white;
+            if (currentIndex == index && usingWords.isNotEmpty) {
+              // Eğer kullanıcının seçtiği kelime bu kutuda gösteriliyorsa ve kelime başında ünlem işareti varsa
+              if (selectedSentenceWords[index].startsWith('!')) {
+                backgroundColor = Colors.green;// Arka plan rengini yeşil yap
+
+              }
+              else{
+               backgroundColor = Colors.red;
+              }
+
+            }
             return Positioned(
               top: 50 + index * 140,
-              child: Container(
-                width: 200,
-                height: 90,
-                decoration: BoxDecoration(
-                  //color: Colors.lime[(index + 1) * 100],
-                  //color: Colors.amber[(index + 1) * 100],
-                  color: Colors.white,
-                  border: Border.all(color: Colors.blueGrey.shade900, width: 2.0),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withOpacity(0.8),
-                      spreadRadius: 5,
-                      blurRadius: 7,
-                      offset: Offset(0, 3),
+              child: GestureDetector(
+                onTap: () {
+                  setState(() {
+                    currentIndex = index;
+                    checkAndLoadNextWord();// Seçilen kelimeye göre kontrol yap ve yeni bir kelimeye geç
+
+                  });
+                },
+                child: Container(
+                  width: 200,
+                  height: 90,
+                  decoration: BoxDecoration(
+                    color: backgroundColor,
+                    border: Border.all(color: Colors.blueGrey.shade900, width: 2.0),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.8),
+                        spreadRadius: 5,
+                        blurRadius: 7,
+                        offset: Offset(0, 3),
+                      ),
+                    ],
+                  ),
+                  child: Center(
+                    child: Text(
+                      usingWords.isNotEmpty ? usingWords[index] : '',
+                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
                     ),
-                  ],
+                  ),
                 ),
-                child: Center(child: Text(UsingWords[index])),
               ),
             );
           }),
